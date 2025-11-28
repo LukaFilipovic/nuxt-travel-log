eb64aa5e2a6e98e91398ff75031a551b9af91c0a
LukaFilipovic <l.filipovic2016@gmail.com>
2025-11-28 23:42:36 +0100

    lint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import antfu from '@antfu/eslint-config';
+
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+  antfu({
+    type: 'app',
+    vue: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      quotes: 'single',
+      semi: true,
+      indent: 2,
+    },
+  }, {
+    rules: {
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'antfu/no-top-level-await': 'off',
+      'node/prefer-global/process': 'off',
+      'node/no-process-env': ['error'],
+      'perfectionist/sort-imports': ['error', {
+        tsconfigRootDir: '.',
+      }],
+      'unicorn/filename-case': ['error', {
+        case: 'kebabCase',
+        ignore: ['.nuxt', 'README.md', 'LICENSE'],
+      }],
+    },
+  }),
+);
